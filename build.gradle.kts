94280d23a38c6933c05f052c35ebe0759198e9c6
intsuc <intsuc@gmail.com>
2023-01-27 14:31:36 +0900

    Initialize Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(19)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "elaboration-kt"
