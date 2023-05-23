b005733e8f00585e3308f403bd73b9750be15d6f
intsuc <intsuc@gmail.com>
2023-05-24 03:46:34 +0900

    Use `sealed class` instead of `sealed interface`

--- src/main/kotlin/Core.kt
+++ src/main/kotlin/Core.kt
@@ -1,29 +1,29 @@
-sealed interface Core {
-  data object Type : Core
+sealed class Core {
+  data object Type : Core()
 
   data class Func(
     val name: String?,
     val param: Core,
     val result: Core,
-  ) : Core
+  ) : Core()
 
   data class FuncOf(
     val name: String?,
     val body: Core,
-  ) : Core
+  ) : Core()
 
   data class App(
     val func: Core,
     val arg: Core,
-  ) : Core
+  ) : Core()
 
   data class Let(
     val name: String?,
     val init: Core,
     val body: Core,
-  ) : Core
+  ) : Core()
 
   data class Var(
     val level: Int,
-  ) : Core
+  ) : Core()
 }
--- src/main/kotlin/Surface.kt
+++ src/main/kotlin/Surface.kt
@@ -1,34 +1,34 @@
-sealed interface Surface {
-  data object Type : Surface
+sealed class Surface {
+  data object Type : Surface()
 
   data class Func(
     val name: String?,
     val param: Surface,
     val result: Surface,
-  ) : Surface
+  ) : Surface()
 
   data class FuncOf(
     val name: String?,
     val body: Surface,
-  ) : Surface
+  ) : Surface()
 
   data class App(
     val func: Surface,
     val arg: Surface,
-  ) : Surface
+  ) : Surface()
 
   data class Let(
     val name: String?,
     val init: Surface,
     val body: Surface,
-  ) : Surface
+  ) : Surface()
 
   data class Var(
     val name: String,
-  ) : Surface
+  ) : Surface()
 
   data class Anno(
     val target: Surface,
     val type: Surface,
-  ) : Surface
+  ) : Surface()
 }
