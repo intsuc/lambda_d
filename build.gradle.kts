7a035b52d010894173a6f8cf1af277b75a516b3f
intsuc <intsuc@gmail.com>
2023-05-24 01:18:47 +0900

    Use kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_19
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.8.21"
+  kotlin("jvm") version "1.8.21"
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-    testImplementation(kotlin("test"))
+  implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(19)
+  jvmToolchain(19)
+}
+
+tasks.withType<KotlinCompile> {
+  compilerOptions {
+    languageVersion.set(KOTLIN_1_9)
+    jvmTarget.set(JVM_19)
+  }
 }
